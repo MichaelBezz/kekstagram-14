6e7ce87679540e92f0b7754e43fec1db967e5c74
MichaelBezz <michael.bezzaponnyy@gmail.com>
2018-07-05 10:57:37 +0300

    fix bug

--- js/gallery.js
+++ js/gallery.js
@@ -2,24 +2,18 @@
 // модуль для отрисовки миниатюры //
 (function () {
 
-  var MAX_COMMENTS = 5;
-
   var mainPhotocard = document.querySelector('.big-picture');
   var bigPicturesCancel = mainPhotocard.querySelector('.big-picture__cancel');
   var commentShow = mainPhotocard.querySelector('.comments-show');
-  var socialLoadmore = mainPhotocard.querySelector('.social__loadmore');
   // функция, для обработки события открытия + добавление обработки по клавиши esc
   var onOverlayOpen = function () {
     mainPhotocard.classList.remove('hidden');
-    socialLoadmore.addEventListener('click', onSocialCommentsLoadmore);
-    hideSocialComments();
-    hideSocialCommentsLoadmore();
+    getNumberSocialComments();
     document.addEventListener('keydown', onOverlayEscPress);
   };
   // функция, для обработки события закрытия + удаление обработки по клавиши esc
   var onOverlayClose = function () {
     mainPhotocard.classList.add('hidden');
-    socialLoadmore.removeEventListener('click', onSocialCommentsLoadmore);
     document.removeEventListener('keydown', onOverlayEscPress);
   };
   // функция, для обработки события закрытия по esc
@@ -51,36 +45,6 @@
       }
     }
     commentShow.textContent = commentCounter;
-  };
-  // функция, которая скрывает лишние комментарии
-  var hideSocialComments = function () {
-    var socialComments = getArrayComments();
-    if (socialComments.length > MAX_COMMENTS) {
-      for (var i = MAX_COMMENTS; i < socialComments.length; i++) {
-        socialComments[i].style.display = 'none';
-      }
-    }
-    getNumberSocialComments();
-  };
-  // функция, которая загружает дополнительные комментарии
-  var onSocialCommentsLoadmore = function () {
-    var socialComments = getArrayComments();
-    socialComments.filter(function (comment) {
-      return comment.style.display === 'none';
-    });
-
-    for (var i = 0; i < socialComments.length; i++) {
-      socialComments[i].style.display = '';
-    }
-    socialLoadmore.classList.add('hidden');
-    getNumberSocialComments();
-  };
-  // функция, которая скрывает кнопку загрузки дополнительных комментариев
-  var hideSocialCommentsLoadmore = function () {
-    var socialComments = getArrayComments();
-    if (socialComments.length <= MAX_COMMENTS) {
-      socialLoadmore.classList.add('hidden');
-    }
   };
 
   window.gallery = {
